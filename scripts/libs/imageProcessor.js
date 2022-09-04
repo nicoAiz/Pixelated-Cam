8a8e8d44e012a9579342d36c41e784d65969d0b5
Nico <nicogu.safe@gmail.com>
2022-09-03 22:19:51 -0300

    Refactored some code and added a description

--- scripts/libs/imageProcessor.js
+++ scripts/libs/imageProcessor.js
@@ -1,26 +1,23 @@
-function removeColor(img, r = 0, g = 255, b = 0, threshold = 10) {
-  const thresholdSquared = threshold * threshold
+// Gustavo Nicoletti
+
+// Experiment image manipulation library
+// v0.0.1 [03/09/2022]
+// - Initial state
+
+function iP_forEachPixel(img, callback) {
   const canvas = document.createElement('canvas')
   const ctx = canvas.getContext('2d')
   canvas.width = img.width
   canvas.height = img.height
-  try {
-    ctx.drawImage(img, 0, 0)
-    let imgData = ctx.getImageData(0, 0, canvas.width, canvas.height)
+  ctx.drawImage(img, 0, 0)
+  const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height)
+  let d = imageData.data
 
-    for (let i = 0; i < imgData.data.length; i += 4) {
-      let pR = imgData.data[i]
-      let pG = imgData.data[i + 1]
-      let pB = imgData.data[i + 2]
-      
-      if ((pR - r)**2 + (pG - g)**2 + (pB - b)**2 <= thresholdSquared)
-        imgData.data[i + 3] = 0
-    }
+  for (let i = 0; i < d.length; i += 4)
+    [d[i], d[i + 1], d[i + 2], d[i + 3]] = callback([d[i], d[i + 1], d[i + 2], d[i + 3]], i)
 
-    ctx.putImageData(imgData, 0, 0)
-    let newImg = loadImage(canvas.toDataURL())
-    return newImg
-  } catch(err) { console.error(err) }
+  ctx.putImageData(imageData, 0, 0)
+  return loadImage(canvas.toDataURL())
 }
 
 function pixelateImage(img, rez) {
@@ -28,107 +25,69 @@ function pixelateImage(img, rez) {
   const ctx = canvas.getContext('2d')
   canvas.width = img.width * rez
   canvas.height = img.height * rez
-  try {
-    ctx.drawImage(img, 0, 0, canvas.width, canvas.height)
-    let imgData = ctx.getImageData(0, 0, canvas.width, canvas.height)
+  ctx.drawImage(img, 0, 0, canvas.width, canvas.height)
+  const imgData = ctx.getImageData(0, 0, canvas.width, canvas.height)
+  ctx.putImageData(imgData, 0, 0)
+  const newImg = loadImage(canvas.toDataURL())
+  return newImg
+}
 
-    // Do whatever here...
-
-    ctx.putImageData(imgData, 0, 0)
-    let newImg = loadImage(canvas.toDataURL())
-    return newImg
-  } catch(err) { console.error(err) }
+function removeColor(img, r = 0, g = 255, b = 0, threshold = 10) {
+  const thresholdSquared = threshold * threshold
+  return iP_forEachPixel(img, p => {
+    if ((p[0] - r)**2 + (p[1] - g)**2 + (p[2] - b)**2 <= thresholdSquared)
+      p[3] = 0
+    return p
+  })
 }
 
 function decreasePalette(img, colors) {
-  const canvas = document.createElement('canvas')
-  const ctx = canvas.getContext('2d')
-  canvas.width = img.width
-  canvas.height = img.height
-  try {
-    ctx.drawImage(img, 0, 0)
-    let imgData = ctx.getImageData(0, 0, canvas.width, canvas.height)
-
-    for (let i = 0; i < imgData.data.length; i += 4) {
-      imgData.data[i]     = Math.floor(imgData.data[i] / colors) * colors
-      imgData.data[i + 1] = Math.floor(imgData.data[i + 1] / colors) * colors
-      imgData.data[i + 2] = Math.floor(imgData.data[i + 2] / colors) * colors
-    }
-
-    ctx.putImageData(imgData, 0, 0)
-    let newImg = loadImage(canvas.toDataURL())
-    return newImg
-  } catch(err) { console.error(err) }
+  return iP_forEachPixel(img, p => {
+    p[0] = Math.floor(p[0] / colors) * colors
+    p[1] = Math.floor(p[1] / colors) * colors
+    p[2] = Math.floor(p[2] / colors) * colors
+    return p
+  })
 }
 
 function usePalette(img, palette) {
-  const canvas = document.createElement('canvas')
-  const ctx = canvas.getContext('2d')
-  canvas.width = img.width
-  canvas.height = img.height
-  try {
-    ctx.drawImage(img, 0, 0)
-    let imgData = ctx.getImageData(0, 0, canvas.width, canvas.height)
+  return iP_forEachPixel(img, p => {
+    let closestColor = null
+    let closestDistance = 999999
 
-    for (let i = 0; i < imgData.data.length; i += 4) {
-      let pR = imgData.data[i]
-      let pG = imgData.data[i + 1]
-      let pB = imgData.data[i + 2]
-
-      let closestColor = null
-      let closestDistance = 999999
-
-      for (let color of palette) {
-        const d = distanceND(color, [pR, pG, pB])
-        if (d < closestDistance) {
-          closestDistance = d
-          closestColor = color
-        }
+    for (let color of palette) {
+      const d = distanceND(color, p)
+      if (d < closestDistance) {
+        closestDistance = d
+        closestColor = color
       }
-
-      if (!closestColor) continue
-
-      imgData.data[i]     = closestColor[0]
-      imgData.data[i + 1] = closestColor[1]
-      imgData.data[i + 2] = closestColor[2]
     }
 
-    ctx.putImageData(imgData, 0, 0)
-    let newImg = loadImage(canvas.toDataURL())
-    return newImg
-  } catch(err) { console.error(err) }
+    if (closestColor) {
+      p[0] = closestColor[0]
+      p[1] = closestColor[1]
+      p[2] = closestColor[2]
+    }
+    
+    return p
+  })
 }
 
 function getPixelsCluster(img, r, g, b, threshold) {
-  const thresholdSquared = threshold * threshold
-  const canvas = document.createElement('canvas')
-  const ctx = canvas.getContext('2d')
-  canvas.width = img.width
-  canvas.height = img.height
   const pixelsFound = []
-  try {
-    ctx.drawImage(img, 0, 0)
-    let imgData = ctx.getImageData(0, 0, canvas.width, canvas.height)
+  const thresholdSquared = threshold * threshold
+  iP_forEachPixel(img, (p, i) => {
+    if ((p[0] - r)**2 + (p[1] - g)**2 + (p[2] - b)**2 <= thresholdSquared)
+      pixelsFound.push([(i / 4) % img.width, floor(i / 4 / img.width)])
+    return p
+  })
 
-    for (let i = 0; i < imgData.data.length; i += 4) {
-      let pR = imgData.data[i]
-      let pG = imgData.data[i + 1]
-      let pB = imgData.data[i + 2]
-      
-      if ((pR - r)**2 + (pG - g)**2 + (pB - b)**2 <= thresholdSquared) {
-        const x = (i / 4) % canvas.width
-        const y = floor(i / 4 / canvas.width)
-        pixelsFound.push([x, y])
-      }
-    }
-
-    return pixelsFound
-  } catch(err) { console.error(err) }
+  return pixelsFound
 }
 
 function getPixelsClusterCenter(img, r, g, b, threshold) {
   const pixelsCluster = getPixelsCluster(img, r, g, b, threshold)
-  let center = [0, 0]
+  const center = [0, 0]
   for (let p of pixelsCluster) {
     center[0] += p[0]
     center[1] += p[1]
@@ -136,6 +95,5 @@ function getPixelsClusterCenter(img, r, g, b, threshold) {
 
   center[0] /= pixelsCluster.length
   center[1] /= pixelsCluster.length
-  
   return center
 }
