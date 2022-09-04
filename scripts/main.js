bb89d22e9dd0ae172c32d63c121d62ced64338ee
Nico <nicogu.safe@gmail.com>
2022-09-04 18:39:51 -0300

    Added upload and download actions and rewrote some code

--- scripts/main.js
+++ scripts/main.js
@@ -7,8 +7,9 @@ const oCanvas = document.createElement('canvas')
 const oCtx = oCanvas.getContext('2d')
 let resultImage
 
-// Does nothing at the moment...
-const palette = new Array(256).fill([0, 0, 0]).map(i => i.map(c => random(255)))
+// Palette to be used in the result image
+const palette = [[222,57,95],[176,230,38],[222,45,84],[44,155,128],[37,178,72],[190,170,172],[86,139,213],[185,253,189],[85,8,63],[134,64,154],[195,232,40],[114,140,55],[115,178,74],[195,96,115],[69,69,248],[86,37,243],[43,114,134],[99,24,221],[140,228,180],[76,64,11],[164,180,37],[15,47,112],[147,130,13],[113,152,9],[197,6,244],[24,196,123],[175,254,252],[199,253,215],[98,19,241],[147,196,102],[201,202,72],[247,52,230],[167,0,68],[236,253,235],[200,115,44],[100,58,172],[63,150,222],[116,52,198],[165,12,217],[179,193,60],[144,98,129],[84,222,248],[233,133,29],[173,224,12],[108,150,11],[173,215,13],[52,39,33],[207,252,39],[85,211,198],[214,95,193],[251,177,119],[36,0,244],[151,235,139],[195,229,13],[74,91,121],[154,123,127],[142,55,130],[228,127,152],[18,83,213],[145,61,0],[199,18,117],[171,185,177],[219,187,190],[132,188,137]]
+const palette2 = new Array(64).fill([0, 0, 0]).map(i => i.map(c => random(255)))
 
 // Use webcam as source for effect
 // otherwise, use example.jpeg
@@ -24,30 +25,57 @@ navigator.mediaDevices.getUserMedia({ video: true, audio: false })
   }).catch(err => {
     const image = loadImage('images/example.jpeg', () => {
       source = image
-      oCanvas.width  = image.width
-      oCanvas.height = image.height
+      oCanvas.width  = source.width
+      oCanvas.height = source.height
       draw()
     })
   })
+
+// File upload handling
+const uploadImage = document.getElementById('upload-image')
+uploadImage.addEventListener('change', () => {
+  const file = uploadImage.files[0]
+
+  if (!file.type.match(/image-*/))
+    return alert('Invalid format')
+
+  const image = loadImage(URL.createObjectURL(file), () => {
+    source = image
+    oCanvas.width  = source.width
+    oCanvas.height = source.height
+  })
+})
+
+// Download handling
+const downloadImage = document.getElementById('download-image')
+downloadImage.addEventListener('click', () => {
+  const anchor = document.createElement('a')
+  anchor.download = 'image.png'
+  anchor.href = canvas.toDataURL('image/png').replace('image/png', 'image/octet-stream')
+  anchor.click()
+})
 
 async function draw() {
   useContext(oCtx)
   drawImage(source, 0, 0, oCanvas.width, oCanvas.height)
 
-  resultImage = await iP_pixelateImage(oCanvas, 0.16) // Lower the resolution by 0.16
-  resultImage = await iP_decreasePalette(resultImage, 48) // 'Snap' colors to a palette of size 48
-  resultImage = await iP_usePalette(resultImage, []) // 'Snap' colors to a palette, currently unused
+  resultImage = oCanvas
+  resultImage = await iP_pixelateImage(resultImage, 100) // Lower the resolution to maximum n pixels
+  // resultImage = await iP_decreasePalette(resultImage, 1) // 'Snap' colors to a palette of size n
+  resultImage = await iP_usePalette(resultImage, palette) // 'Snap' colors to a palette, currently unused
 
   // Scaling canvas properly to the screen
-  canvas.width  = width  = innerWidth
-  canvas.height = height = innerHeight
-  const scale = width / oCanvas.width < height / oCanvas.height ? width / oCanvas.width : height / oCanvas.height
+  canvas.width  = innerWidth
+  canvas.height = innerHeight
+  const scale = innerWidth / oCanvas.width < innerHeight / oCanvas.height ? innerWidth / oCanvas.width : innerHeight / oCanvas.height
+  canvas.width  = width  = oCanvas.width * scale
+  canvas.height = height = oCanvas.height * scale
 
   ctx.imageSmoothingEnabled  = false
   oCtx.imageSmoothingEnabled = false
 
   useContext(ctx)
   background('#000')  
-  drawImage(resultImage, (width - oCanvas.width * scale) / 2, (height - oCanvas.height * scale) / 2, oCanvas.width * scale, oCanvas.height * scale)
+  drawImage(resultImage, 0, 0, width, height)
   draw()
 }
