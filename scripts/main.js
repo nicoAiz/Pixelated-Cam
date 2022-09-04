41b5e7e2caa25018a2bfa04d2dafc7608b1415ab
Nico <nicogu.safe@gmail.com>
2022-09-04 12:45:41 -0300

    Fixed naming

--- scripts/main.js
+++ scripts/main.js
@@ -2,7 +2,7 @@ const { canvas, ctx } = createCanvasContext(innerWidth, innerHeight)
 let width  = canvas.width
 let height = canvas.height
 
-let video = document.createElement('video')
+let source = document.createElement('video')
 const oCanvas = document.createElement('canvas')
 const oCtx = oCanvas.getContext('2d')
 let resultImage
@@ -14,16 +14,16 @@ const palette = new Array(256).fill([0, 0, 0]).map(i => i.map(c => random(255)))
 // otherwise, use example.jpeg
 navigator.mediaDevices.getUserMedia({ video: true, audio: false })
   .then(rawData => {
-    video.srcObject = rawData
-    video.play()
-    video.onloadeddata = () => {
-      oCanvas.width  = video.videoWidth
-      oCanvas.height = video.videoHeight
+    source.srcObject = rawData
+    source.play()
+    source.onloadeddata = () => {
+      oCanvas.width  = source.videoWidth
+      oCanvas.height = source.videoHeight
       draw()
     }
   }).catch(err => {
     const image = loadImage('images/example.jpeg', () => {
-      video = image
+      source = image
       oCanvas.width  = image.width
       oCanvas.height = image.height
       draw()
@@ -32,7 +32,7 @@ navigator.mediaDevices.getUserMedia({ video: true, audio: false })
 
 async function draw() {
   useContext(oCtx)
-  drawImage(video, 0, 0, oCanvas.width, oCanvas.height)
+  drawImage(source, 0, 0, oCanvas.width, oCanvas.height)
 
   resultImage = await iP_pixelateImage(oCanvas, 0.16) // Lower the resolution by 0.16
   resultImage = await iP_decreasePalette(resultImage, 48) // 'Snap' colors to a palette of size 48
